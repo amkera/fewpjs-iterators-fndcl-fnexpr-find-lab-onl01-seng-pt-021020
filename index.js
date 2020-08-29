b286ca3782baee64d90a4327036f60c41a3680f1
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-29 04:46:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+//There is a difference between find and filter. Find returns the first value for which the callback function returns true, but filter returns all of them. 
+
