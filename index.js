2dcd33e8c52a9511da1c71c677319b69dc52b155
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-29 04:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function testFunc() {
 
 //There is a difference between find and filter. Find returns the first value for which the callback function returns true, but filter returns all of them. 
 
+// let roommates = ["jess", "winston", "winston", "nick"];
+ 
+// roommates.find( function(s) { return s === "winston" })
+// //=> winston
+ 
+// roommates.filter(function(s) { return s === "winston" })
+// //=> [winston, winston]
